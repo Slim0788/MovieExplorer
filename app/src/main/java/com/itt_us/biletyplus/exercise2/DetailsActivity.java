2b5472c0bb11d3e4e5045e5a9c1d92f0a166f10b
Slim <serge0788@gmail.com>
2019-09-23 16:03:49 +0300

    Initial commit

---
+++ app/src/main/java/com/itt_us/biletyplus/exercise2/DetailsActivity.java
@@ -0,0 +1,28 @@
+package com.itt_us.biletyplus.exercise2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private static final String trailerURL = "https://www.youtube.com/watch?v=6ZfuNTqbHE8";
+
+    private Button movieTrailerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        setTitle("Academy Minsk Movies");
+
+        movieTrailerButton = findViewById(R.id.details_btn_trailer);
+
+        movieTrailerButton.setOnClickListener(v ->
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(trailerURL))));
+    }
+
+}
