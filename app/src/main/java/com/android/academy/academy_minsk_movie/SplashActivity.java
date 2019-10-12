656d4f11bcb0428bef85e38fdaa80d9305770c40
Slim <serge0788@gmail.com>
2019-10-12 19:44:21 +0300

    add splash

---
+++ app/src/main/java/com/android/academy/academy_minsk_movie/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.android.academy.academy_minsk_movie;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000;
+
+    private final Handler handler = new Handler();
+    private final Launcher launcher = new Launcher();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        handler.postDelayed(launcher, SPLASH_DELAY);
+    }
+
+    @Override
+    protected void onStop() {
+        handler.removeCallbacks(launcher);
+        super.onStop();
+    }
+
+    private void launch() {
+        if (!isFinishing()) {
+            Intent intent = new Intent(this, MainActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            overridePendingTransition(R.anim.splash_fade_in, R.anim.splash_fade_out);
+            finish();
+        }
+    }
+
+    private class Launcher implements Runnable {
+        @Override
+        public void run() {
+            launch();
+        }
+    }
+}
