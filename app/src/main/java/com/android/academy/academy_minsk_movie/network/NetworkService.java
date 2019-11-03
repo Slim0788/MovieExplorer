561df414f8aca7962f8421d5cd40c2aff317ca8d
Slim <serge0788@gmail.com>
2019-11-04 00:21:35 +0300

    add interceptor. don't work :(

--- app/src/main/java/com/android/academy/academy_minsk_movie/network/NetworkService.java
+++ app/src/main/java/com/android/academy/academy_minsk_movie/network/NetworkService.java
@@ -1,9 +1,18 @@
 package com.android.academy.academy_minsk_movie.network;
 
+import com.android.academy.academy_minsk_movie.network.api.TmdbServiceApi;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-import com.android.academy.academy_minsk_movie.network.api.TmdbServiceApi;
 
 public class NetworkService {
 
@@ -11,13 +20,17 @@ public class NetworkService {
     private static final String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
 
     private static NetworkService instance;
+    private static int pageNumber = 1;
 
     private Retrofit retrofit;
+    private OkHttpClient client;
 
     private NetworkService() {
+        createClient();
         retrofit = new Retrofit.Builder()
                 .baseUrl(TMDB_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+//                .client(client)
                 .build();
     }
 
@@ -26,6 +39,30 @@ public class NetworkService {
             instance = new NetworkService();
         }
         return instance;
+    }
+
+    private void createClient() {
+        client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @NotNull
+                    @Override
+                    public Response intercept(@NotNull Chain chain) throws IOException {
+                        Request original = chain.request();
+                        HttpUrl originalHttpUrl = original.url();
+
+                        HttpUrl url = originalHttpUrl.newBuilder()
+                                .addQueryParameter("page", String.valueOf(pageNumber))
+                                .build();
+
+                        // Request customization: add request headers
+                        Request.Builder requestBuilder = original.newBuilder()
+                                .url(url);
+
+                        Request request = requestBuilder.build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+        pageNumber++;
     }
 
     public TmdbServiceApi getJsonApi() {
